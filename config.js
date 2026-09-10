9af0c137ac9638040878bebc2b6301bd05598bff
oandre-lima <aal.lima2@gmail.com>
2026-09-10 12:44:35 -0300

    chore: advance public smoke tests to 6c-v2

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.SMOKE_CONFIG={experimentVersion:"6c-v1",supabaseUrl:"https://kmdtgthxpazqdrqcpnro.supabase.co",publishableKey:"sb_publishable_B1g4jlU1uHhJln8r3YtsGQ_rsdnKgNO"};
+window.SMOKE_CONFIG={experimentVersion:"6c-v2",supabaseUrl:"https://kmdtgthxpazqdrqcpnro.supabase.co",publishableKey:"sb_publishable_B1g4jlU1uHhJln8r3YtsGQ_rsdnKgNO"};
